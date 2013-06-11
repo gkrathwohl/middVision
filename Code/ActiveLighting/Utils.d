872081510538d25284087bd213c1fa59b7fab5b7
Greg Krathwohl <gkrathwohl@middlebury.edu>
2013-06-11 14:46:28 -0400

    fixed relative directories in control

---
+++ Code/ActiveLighting/Utils.d
@@ -0,0 +1,15 @@
+Utils.d: ../Utils.cpp ../imageLib/imageLib.h ../imageLib/Error.h \
+ ../imageLib/Image.h ../imageLib/RefCntMem.h ../imageLib/ImageIO.h \
+ ../imageLib/Convert.h
+
+../imageLib/imageLib.h:
+
+../imageLib/Error.h:
+
+../imageLib/Image.h:
+
+../imageLib/RefCntMem.h:
+
+../imageLib/ImageIO.h:
+
+../imageLib/Convert.h:
