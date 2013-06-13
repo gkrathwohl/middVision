bd97cf810dcafdfdf4d4f9f5f1a6f9ae3d60bf01
Greg Krathwohl <gkrathwohl@middlebury.edu>
2013-06-13 10:28:53 -0400

    removed debug folders

--- Code/ActiveLighting/Debug/imageLib/ImageIOpng.d
+++
@@ -1,8 +0,0 @@
-imageLib/ImageIOpng.d: ../imageLib/ImageIOpng.cpp ../imageLib/Image.h \
- ../imageLib/RefCntMem.h ../imageLib/Error.h
-
-../imageLib/Image.h:
-
-../imageLib/RefCntMem.h:
-
-../imageLib/Error.h:
