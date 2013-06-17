f2905a085172875175a5a63da9bffb41aa22d625
Greg Krathwohl <gkrathwohl@middlebury.edu>
2013-06-17 15:45:48 -0400

    still trying to merge

--- Code/ActiveLightingControl.py
+++ Code/ActiveLightingControl.py
@@ -354,8 +354,6 @@ if __name__ == '__main__':
             safemkdirs(outputDir)
             execute("ActiveLighting/ActiveLighting4 confidence "+outputDir+" "+folder+"/*")
         
-        
-        
         answer = ""
         if(args.auto):
             step = "threshold"
